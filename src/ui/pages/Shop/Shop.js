2976dae1a61b035d188722aad6195fd95266ceef
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-08-24 21:46:37 +0200

    Cleanup Button

--- src/ui/pages/Shop/Shop.js
+++ src/ui/pages/Shop/Shop.js
@@ -5,43 +5,58 @@ import SliderDiv from './components/SliderDiv';
 import SliderItem from './components/SliderItem';
 import Nav from './components/Nav';
 export default function Shop() {
-    var settings = {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-
     };
+
+    const items = [
+        {
+            src:
+                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1522098543979-ffc7f79a56c4%3Fixlib%3Drb-1.2.1%26q%3D80%26fm%3Djpg%26crop%3Dentropy%26cs%3Dtinysrgb%26w%3D1080%26fit%3Dmax%26ixid%3DeyJhcHBfaWQiOjEyMDd9&f=1&nofb=1',
+            title: 'Syze Dielli',
+            price: '30',
+            description:
+                'Lorem ipsum dolor sit amet consectetur, adipisicing\n' +
+                '                        elit. Odit ducimus accusantium vitae iure harum, ipsum\n' +
+                '                        maxime debitis voluptas explicabo reprehenderit! Magnam\n' +
+                '                        harum ex laborum, earum iure doloremque necessitatibus\n' +
+                '                        voluptates assumenda.',
+        },
+        {
+            src:
+                'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.takeshape.io%2F1f1d0876-be74-4b33-99c8-6ac93f1d70db%2Fdev%2Fb998e714-c99e-463e-af27-fca874b3a881%2Fhenrik-hansen-1415978-unsplash.jpg%3Fauto%3Dcompress%252Cformat&f=1&nofb=1',
+            title: 'Syze Luani',
+            price: '40',
+            description:
+                'Lorem ipsum dolor sit amet consectetur, adipisicing\n' +
+                '                        elit. Odit ducimus accusantium vitae iure harum, ipsum\n' +
+                '                        maxime debitis voluptas explicabo reprehenderit! Magnam\n' +
+                '                        harum ex laborum, earum iure doloremque necessitatibus\n' +
+                '                        voluptates assumenda.',
+        },
+    ];
+
     return (
         <Box as="div" width="100%" height="100vh">
-            <Nav></Nav>
+            <Nav />
+
             <SliderDiv>
                 <Slider {...settings}>
-                    <SliderItem
-                        imageSrc="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1522098543979-ffc7f79a56c4%3Fixlib%3Drb-1.2.1%26q%3D80%26fm%3Djpg%26crop%3Dentropy%26cs%3Dtinysrgb%26w%3D1080%26fit%3Dmax%26ixid%3DeyJhcHBfaWQiOjEyMDd9&f=1&nofb=1"
-                        title="Syze Dielli"
-                        price="30"
-                    >
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Odit ducimus accusantium vitae iure harum, ipsum
-                        maxime debitis voluptas explicabo reprehenderit! Magnam
-                        harum ex laborum, earum iure doloremque necessitatibus
-                        voluptates assumenda.
-                    </SliderItem>
-
-                    <SliderItem
-                        imageSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.takeshape.io%2F1f1d0876-be74-4b33-99c8-6ac93f1d70db%2Fdev%2Fb998e714-c99e-463e-af27-fca874b3a881%2Fhenrik-hansen-1415978-unsplash.jpg%3Fauto%3Dcompress%252Cformat&f=1&nofb=1"
-                        title="Syze Luani"
-                        price="40"
-                    >
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Odit ducimus accusantium vitae iure harum, ipsum
-                        maxime debitis voluptas explicabo reprehenderit! Magnam
-                        harum ex laborum, earum iure doloremque necessitatibus
-                        voluptates assumenda.
-                    </SliderItem>
+                    {items.map(item => (
+                        <SliderItem
+                            imageSrc={item.src}
+                            title={item.title}
+                            price={item.price}
+                        >
+                            {item.description}
+                        </SliderItem>
+                    ))}
                 </Slider>
+                }
             </SliderDiv>
         </Box>
     );
