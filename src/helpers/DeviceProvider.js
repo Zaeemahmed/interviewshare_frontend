f0800cd0873c96615d3d417d288b27ece2ae267a
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-08-30 16:54:37 +0200

    Fix Cleaup

--- src/helpers/DeviceProvider.js
+++ src/helpers/DeviceProvider.js
@@ -1,17 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import TemplateDesktop from '../ui/templates/TemplateDesktop';
-import TemplateMobile from '../ui/templates/TemplateMobile';
-import { SizeContext } from '../context/SizeContext';
 
 export default function DeviceProvider({ children }) {
-    const isDesktop = useContext(SizeContext);
     return (
         <>
-            {isDesktop ? (
-                <TemplateDesktop>{children}</TemplateDesktop>
-            ) : (
-                <TemplateMobile>{children}</TemplateMobile>
-            )}
+            <TemplateDesktop>{children}</TemplateDesktop>
         </>
     );
 }
--- src/ui/components/Base/Grid.js
+++ src/ui/components/Base/Grid.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { Grid as MuiGrid } from '@material-ui/core';
 import {
