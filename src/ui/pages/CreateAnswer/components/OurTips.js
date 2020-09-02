1ee02bf3ff0313273aa0b20b0b1c73a87168f0d8
zaeemAhmed542 <zahmed.bscs18seecs@seecs.edu.pk>
2020-09-02 22:57:00 +0500

    added collapse functionality to TIPS component

--- src/ui/pages/CreateAnswer/components/OurTips.js
+++ src/ui/pages/CreateAnswer/components/OurTips.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Box, Text } from '../../../components/Base/Base';
+import React, { useState } from 'react';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Box, Text, Flex } from '../../../components/Base/Base';
 import Tip from './Tip';
 
 const OurTips = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const handleCollapse = () => {
+        if (collapsed) {
+            setCollapsed(false);
+        } else {
+            setCollapsed(true);
+        }
+    };
     return (
         <Box
             backgroundColor="#EFF0F0"
@@ -10,12 +21,30 @@ const OurTips = () => {
             borderRadius="8px"
             mt="2rem"
         >
-            <Text color="#A1A5A5" font-weight="bold" fontFamily="Roboto">
-                OUR TIPS
-            </Text>
-            <Tip tip="Describe the technology"/>
-            <Tip tip="Describe your team (the sizes and roles your team had)"/>
-            <Tip tip="Describe Your achievements"/>
+            <Flex justifyContent="space-between" alignItems="center">
+                <Text color="#A1A5A5" font-weight="bold" fontFamily="Roboto">
+                    OUR TIPS
+                </Text>
+                {collapsed ? (
+                    <ExpandMoreIcon
+                        style={{ color: '#2074D5' }}
+                        onClick={handleCollapse}
+                    />
+                ) : (
+                    <ExpandLessIcon
+                        style={{ color: '#2074D5' }}
+                        onClick={handleCollapse}
+                    />
+                )}
+            </Flex>
+
+            {!collapsed ? (
+                <Box>
+                    <Tip tip="Describe the technology" />
+                    <Tip tip="Describe your team (the sizes and roles your team had)" />
+                    <Tip tip="Describe Your achievements" />
+                </Box>
+            ) : null}
         </Box>
     );
 };
