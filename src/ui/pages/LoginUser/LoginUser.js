9fccfa9cb370c1390bccba4f39f1069ee2fed354
Edes2128 <guerrilsi46@gmail.com>
2020-09-06 13:57:36 +0200

    Clean some stuff

--- src/ui/pages/LoginUser/LoginUser.js
+++ src/ui/pages/LoginUser/LoginUser.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Box } from '../../components/Base/Base';
+import { Box, Flex } from '../../components/Base/Base';
 import { useForm } from 'react-hook-form';
+import { TextField } from '@material-ui/core';
+import theme from '../../../config/theme';
 
 export default function LoginUser() {
     const { errors, handleSubmit, register } = useForm();
@@ -12,15 +14,15 @@ export default function LoginUser() {
             </Box>
 
             <form onSubmit={handleSubmit(onSubmit)} style={{ color: 'red' }}>
-                <Box display="flex" flexDirection="column">
+                <Flex flexDirection="column">
                     <label style={{ color: '#777D7D' }}>Email</label>
-                    <input
+                    <TextField
                         required={true}
                         style={{
                             borderRadius: '8px',
                             padding: '14px',
-                            background: '#F5F5F5',
-                            borderColor: '#F5F5F5',
+                            background: theme.colors.bgGrayLight,
+                            borderColor: theme.colors.bgGrayLight,
                             outline: 'none',
                         }}
                         name="email"
@@ -33,16 +35,16 @@ export default function LoginUser() {
                         })}
                     />
                     {errors.email && errors.email.message}
-                </Box>
-                <Box display="flex" flexDirection="column">
+                </Flex>
+                <Flex flexDirection="column">
                     <label style={{ color: '#777D7D' }}>Password</label>
-                    <input
+                    <TextField
                         required={true}
                         style={{
                             borderRadius: '8px',
                             padding: '14px',
-                            background: '#F5F5F5',
-                            borderColor: '#F5F5F5',
+                            background: theme.colors.bgGrayLight,
+                            borderColor: theme.colors.bgGrayLight,
                             outline: 'none',
                         }}
                         minLength="10"
@@ -57,12 +59,12 @@ export default function LoginUser() {
                         })}
                     />
                     {errors.password && errors.password.message}
-                </Box>
+                </Flex>
 
                 <button
                     style={{
                         marginTop: '60px',
-                        background: '#2074D5',
+                        background: theme.colors.btnBlue,
                         borderRadius: '31px',
                         color: 'white',
                         padding: '10px 40px',
