b201933afba619882d8d9fb6a19266f78d240cc9
Edes2128 <guerrilsi46@gmail.com>
2020-09-08 21:55:36 +0200

    i18next setup

--- src/helpers/LocaleProvider.js
+++ src/helpers/LocaleProvider.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { IntlProvider } from 'react-intl';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { translations } from '../locales';
 
 const language = (
@@ -10,12 +10,25 @@ const language = (
 const userLocale = ['de', 'en'].includes(language) ? language : 'en';
 const messages = translations[userLocale];
 
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources: {
+            en: {
+                translation: messages,
+            },
+        },
+        lng: userLocale,
+        fallbackLng: 'en',
+
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+
 function LocaleProvider({ children }) {
-    return (
-        <IntlProvider locale={userLocale} messages={messages}>
-            {children}
-        </IntlProvider>
-    );
+    //TODO: get dynamic label translations via GraphQL Query
+
+    return children;
 }
 
 export default LocaleProvider;
